8665c15d0782684ca990069160beae305c79877d
Florian Sloot <flosloot@gmx.de>
2020-06-08 15:46:18 +0200

    [AUTH] dont fetch if not authenticated

--- src/context/Stack.js
+++ src/context/Stack.js
@@ -1,12 +1,16 @@
 import React from "react";
 import axios from "axios";
+import { AuthContext } from "./Auth";
 export const StackContext = React.createContext();
 
 export const StackProvider = ({ children }) => {
+  const { authenticated } = React.useContext(AuthContext);
   const [stacks, setStacks] = React.useState([]);
 
   React.useEffect(() => {
     (async () => {
+      if (!authenticated) return;
+
       const { data } = await axios.get(
         process.env.REACT_APP_API_BASE_URL + "/jam",
         {
